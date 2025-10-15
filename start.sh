8c42481e5c30f27e9857dfdf8528ac76a4fc2b99
Taaevmg <Gan.Gan030406@gmail.com>
2025-10-15 20:11:21 +0000

    Авто-коммит FabricSrver_1.21 15.10.2025 20:11:21

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-java -Xmx6G -Xms3G -jar fabric-server-mc.1.21-loader.0.17.3-launcher.1.1.0.jar nogui
+java -Xmx10G -Xms5G -Dserver.icon=server-icon.png -jar fabric-server-mc.1.21-loader.0.17.3-launcher.1.1.0.jar nogui
